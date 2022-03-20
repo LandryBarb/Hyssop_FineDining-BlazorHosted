876e47edfc5e8dffa37b9e81302cfd235ac52224
Landry <landry@landrybarb.com>
2022-03-20 09:41:25 -0700

    Added MealType Service

---
+++ Hyssop/Server/Services/MealTypeService/IMealTypeService.cs
@@ -0,0 +1,7 @@
+﻿namespace Hyssop.Server.Services.MealTypeService
+{
+    public interface IMealTypeService
+    {
+        Task<ServiceResponse<List<MealType>>> GetAllMealTypes();
+    }
+}
---
+++ Hyssop/Server/Services/MealTypeService/MealTypeService.cs
@@ -0,0 +1,21 @@
+﻿namespace Hyssop.Server.Services.MealTypeService
+{
+    public class MealTypeService : IMealTypeService
+    {
+        private readonly HyssopDbContext _db;
+        public MealTypeService(HyssopDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<ServiceResponse<List<MealType>>> GetAllMealTypes()
+        {
+            var mealTypes = await _db.MealTypes.ToListAsync();
+            return new ServiceResponse<List<MealType>>
+            {
+                Data = mealTypes,
+            };
+        }
+
+
+    }
+}
