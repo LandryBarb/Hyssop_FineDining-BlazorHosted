93ebd5be1e5de9343f1c4d402045c00ea76902fa
Landry <landry@landrybarb.com>
2022-03-20 19:35:17 -0700

    Landing page- Complete

---
+++ Hyssop/Shared/NavMenuClass.cs
@@ -0,0 +1,9 @@
+﻿namespace Hyssop.Shared
+{
+    public class NavMenuClass
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Url { get; set; }
+    }
+}
